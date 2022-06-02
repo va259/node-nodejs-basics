0028a9c2c1410a767f4eea25a3a8c10caaace6d6
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-02 23:13:39 +0200

    copy.js initial

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,27 @@
+import fsPromises from 'node:fs/promises';
+import path from 'node:path'; 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const srcDir = path.join(__dirname, '/files');
+const destDir = path.join(__dirname, '/files_copy');
+
 export const copy = async () => {
-    // Write your code here 
+  const ensureExists = async (dir) => {
+    return new Promise((resolve, reject) => {
+      fsPromises.access(dir)
+      .then(() => resolve(true))
+      .catch(() => reject("FS operation failed"))
+    })
+  }
+
+  ensureExists(srcDir)
+  .then( () => {
+    fsPromises.cp(srcDir, destDir, {errorOnExist: true, force: false,
+      recursive: true}).catch(
+        () => console.error("FS operation failed")
+      )
+  })
+  .catch( (err) => console.error(err) )
 };
