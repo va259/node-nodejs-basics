459e068ec3a07f3df3813aa52922f3e41d948dc5
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-03 04:37:09 +0200

    list.js initial

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,29 @@
+import fs from 'node:fs';
+import path from 'node:path'; 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dir = path.join(__dirname, '/files');
+
 export const list = async () => {
-    // Write your code here 
+  const ensureExists = async (dir) => {
+    return new Promise((resolve, reject) => {
+      fs.access(dir, (err) => {
+        if (err) reject(new Error('FS operation failed'));
+        resolve(true);
+      });
+    })
+  }
+
+  ensureExists(dir)
+  .then(() => {
+    fs.readdir(dir, (err, files) => {
+      if (err) throw err;
+      files.map( file => console.log(file) );
+    })
+  })  
+  .catch((err) => { throw err })
 };
+
+list();
