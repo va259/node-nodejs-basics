94a2d26e1b0b65ba028affab878e7930ceb511b0
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-03 03:42:06 +0200

    read.js initial

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,30 @@
+import fs from 'node:fs';
+import fsPromises from 'node:fs/promises';
+import path from 'node:path'; 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToRead = path.join(__dirname, '/files', 'fileToRead.txt');
+
 export const read = async () => {
-    // Write your code here 
+  const ensureExists = async (file) => {
+    return new Promise((resolve, reject) => {
+      fs.access(file, fs.constants.F_OK, (err) => {
+        if (err) reject(new Error('FS operation failed'));
+        resolve(true);
+      });
+    })
+  }
+
+  ensureExists(fileToRead)
+  .then(() => {
+    fs.readFile(fileToRead, 'utf8', (err, file) => {
+      if (err) {throw err};
+      console.log(file);
+    })
+  })
+  .catch((err) => {throw err})
 };
+
+read();
