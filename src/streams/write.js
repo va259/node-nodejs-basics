25ef7d4dcb206191cefe27fc0e22bae3160254a6
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-05 01:09:37 +0200

    write.js initial

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+import { createWriteStream } from "node:fs";
+import { stdin } from 'node:process';
+
+const readableFromTerminal = stdin;
+const writeableToFile = createWriteStream('./files/fileToWrite.txt', 'utf-8');
+
+readableFromTerminal.pipe(writeableToFile);
+
 export const write = async () => {
-    // Write your code here 
+    readableFromTerminal.on('data', (chunk) => {
+      const chunkStringified = chunk.toString();
+      if (chunkStringified.match('STOP')) readableFromTerminal.unpipe(writeableToFile);
+      return;
+    })
 };
