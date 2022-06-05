f17b97cccb68e3388a910142613ef2355d2eb194
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-05 03:22:14 +0200

    transform.js initial

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,22 @@
+import { stdin, stdout } from 'node:process';
+import { Transform, pipeline } from 'node:stream';  
+
+const readable = stdin;
+const writeable = stdout;
+
 export const transform = async () => {
-    // Write your code here 
+  const transformed = new Transform({
+    transform(chunk, enc, cb) {
+      const chunkStringified = chunk.toString().trim();
+      const reversedChunk = chunkStringified.split('').reverse().join('');
+      cb(null, reversedChunk + '\n');
+    }
+  });
+
+  pipeline(
+    readable,
+    transformed,
+    writeable,
+    err => { throw err }
+  );
 };
