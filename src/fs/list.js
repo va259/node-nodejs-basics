1e86be5c580122249f9018aec3fcf811558414d0
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-05 20:11:08 +0200

    list.js path fixes

--- src/fs/list.js
+++ src/fs/list.js
@@ -4,12 +4,14 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const dir = path.join(__dirname, '/files');
+const dir = path.join(__dirname, 'files');
 
 export const list = async () => {
     readdir(dir, (err, files) => {
-      if (err && err.code === 'ENOENT') throw new Error('FS operation failed');
-      else if (err) throw err;
+      if (err && err.code === 'ENOENT') throw new Error('FS operation failed')
+      else if (err) throw err ;
       console.log(files);
     })
 };
+
+list();
