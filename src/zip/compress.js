8859e2d2adc3aa1b95f6f609d3741bdbea64f1c0
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-05 18:28:29 +0200

    compress.js dynamic path added

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,25 @@
+import path from 'node:path'; 
+import { fileURLToPath } from 'url';
+import zlib from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createWriteStream, createReadStream } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToCompress = path.join(__dirname, path.sep, 'files', 'fileToCompress.txt');
+const archive = path.join(__dirname, path.sep, 'files', 'archive.gz');
+
+const srcFile = createReadStream(fileToCompress);
+const destFile = createWriteStream(archive);
+const gzip = zlib.createGzip();
+
 export const compress = async () => {
-    // Write your code here 
+  pipeline(srcFile, gzip, destFile, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
 };
+
+compress();
