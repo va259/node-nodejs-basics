f5fee1a84d7565d67faf616f24a52f503a5a2a29
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-05 18:29:29 +0200

    decompress.js initial

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,25 @@
+import path from 'node:path'; 
+import { fileURLToPath } from 'url';
+import zlib from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createWriteStream, createReadStream } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToCompress = path.join(__dirname, path.sep, 'files', 'fileToCompress.txt');
+const archive = path.join(__dirname, path.sep, 'files', 'archive.gz');
+
+const srcFile = createReadStream(archive);
+const destFile = createWriteStream(fileToCompress);
+const unzip = zlib.createUnzip();
+
 export const decompress = async () => {
-    // Write your code here 
+  pipeline(srcFile, unzip, destFile, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
 };
+
+decompress();
