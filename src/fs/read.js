2cf52deb26766bd64a6ee1e2b3b0139d9bf8620f
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-05 20:38:47 +0200

    read.js error handling fix

--- src/fs/read.js
+++ src/fs/read.js
@@ -4,24 +4,14 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const fileToRead = path.join(__dirname, '/files', 'fileToRead.txt');
+const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
 
 export const read = async () => {
-  const ensureExists = async (file) => {
-    return new Promise((resolve, reject) => {
-      fs.access(file, fs.constants.F_OK, (err) => {
-        if (err) reject(new Error('FS operation failed'));
-        resolve(true);
-      });
-    })
-  }
-
-  ensureExists(fileToRead)
-  .then(() => {
-    fs.readFile(fileToRead, 'utf8', (err, file) => {
-      if (err) {throw err};
-      console.log(file);
-    })
+  fs.readFile(fileToRead, 'utf8', (err, file) => {
+    if (err && err.code === 'ENOENT') throw new Error('FS operation failed')
+    else if (err) throw err;
+    console.log(file);
   })
-  .catch((err) => {throw err})
 };
+
+read();
