52518304051f2dd1cf2623cc6e4b014e5740b908
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-05 20:52:46 +0200

    args.js minor styel fix & function call added

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,9 +1,11 @@
 export const parseArgs = () => {
   const args = process.argv.slice(2);
-  const stringOfArgs = args
-  .map((el, i) => { if (i % 2 === 0) return `${el} is ${args[i + 1]}` })
-  .filter((el) => { return el != null })
-  .join(', ');
+  const strOfArgs = args
+    .map((el, i) => { if (i % 2 === 0) return `${el} is ${args[i + 1]}` })
+    .filter((el) => { return el != null })
+    .join(', ');
 
-  console.log(stringOfArgs);
+  console.log(strOfArgs);
 };
+
+parseArgs();
