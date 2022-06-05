be1a80e69a25b550dbdc5c7624f2456cb1dddb86
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-05 21:07:55 +0200

    write.js path fixes & function call added

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,15 +1,23 @@
+import path from 'node:path'; 
+import { fileURLToPath } from 'url';
 import { createWriteStream } from "node:fs";
 import { stdin } from 'node:process';
 
-const readableFromTerminal = stdin;
-const writeableToFile = createWriteStream('./files/fileToWrite.txt', 'utf-8');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToWrite = path.join(__dirname, 'files', 'fileToWrite.txt');
 
-readableFromTerminal.pipe(writeableToFile);
+const readableFromTerminal = stdin;
+const writeableToFile = createWriteStream(fileToWrite, 'utf-8');
 
 export const write = async () => {
     readableFromTerminal.on('data', (chunk) => {
       const chunkStringified = chunk.toString();
-      if (chunkStringified.match('STOP')) readableFromTerminal.unpipe(writeableToFile);
+      if (chunkStringified.match('stop')) readableFromTerminal.unpipe(writeableToFile);
       return;
     })
+    
+    readableFromTerminal.pipe(writeableToFile);
 };
+
+write();
