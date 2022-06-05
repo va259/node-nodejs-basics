a7da2b230b9c8243cb2ebe02282ef179f97f7046
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-05 20:58:28 +0200

    calcHash.js function call added + minor style fixes

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,30 @@
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs';
+import path from 'node:path'; 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const file = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const readData = async (file) => {
+    return new Promise((resolve, reject) => {
+      readFile(file, 'utf8', (err, data) => {
+        if (err) reject(new Error(err));
+        resolve(data);
+      });
+    })
+  }
+
+  readData(file)
+  .then((data) => {
+    const hash = createHash('sha256')
+      .update(data)
+      .digest('hex');
+    return hash;
+  })
+  .catch((err) => {throw err})
 };
+
+calculateHash();
