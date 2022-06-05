01b4d84f1b5c95fa8f8eee540ccb5f747b90a0d4
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-06-05 20:31:12 +0200

    copy.js minor fixes + DRY refactoring

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -4,14 +4,15 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const srcDir = path.join(__dirname, '/files');
-const destDir = path.join(__dirname, '/files_copy');
+const srcDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files_copy');
+const errorMessage = 'FS operation failed';
 
 export const copy = async () => {
   const ensureExists = async (dir) => {
     return new Promise((resolve, reject) => {
       fs.access(dir, (err) => {
-        if (err) reject(new Error('FS operation failed'));
+        if (err) reject(new Error(errorMessage));
         resolve(true);
       });
     })
@@ -21,8 +22,11 @@ export const copy = async () => {
   .then( () => {
     fs.cp(srcDir, destDir, {errorOnExist: true, force: false, recursive: true}, 
       (err) => {
-        if (err) throw new Error('FS operation failed');
-    })
+        if (err) throw new Error(errorMessage);
+        console.log('Directory was copied');
+      })
   })
   .catch((err) => { throw err })
 };
+
+copy();
